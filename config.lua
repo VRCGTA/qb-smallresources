e9a8b34ef09c9e89e75b075a0d3bc0673c5f5551
yukad2 <myuratq@gmail.com>
2024-03-16 15:05:24 +0900

    Discordの設定

--- config.lua
+++ config.lua
@@ -49,33 +49,29 @@ Config.AIResponse = {
 
 -- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
 Config.Discord = {
-    isEnabled = false,                                     -- If set to true, then discord rich presence will be enabled
-    applicationId = '00000000000000000',                   -- The discord application id
-    iconLarge = 'logo_name',                               -- The name of the large icon
-    iconLargeHoverText = 'This is a Large icon with text', -- The hover text of the large icon
-    iconSmall = 'small_logo_name',                         -- The name of the small icon
-    iconSmallHoverText = 'This is a Small icon with text', -- The hover text of the small icon
+    isEnabled = true,                                     -- If set to true, then discord rich presence will be enabled
+    applicationId = '1216359653567496192',                   -- The discord application id
+    iconLarge = 'vrcgta_logo',                               -- The name of the large icon
+    iconLargeHoverText = 'GTA RP Server for VRChat Player', -- The hover text of the large icon
+    iconSmall = 'green_icon',                         -- The name of the small icon
+    iconSmallHoverText = 'Playing', -- The hover text of the small icon
     updateRate = 60000,                                    -- How often the player count should be updated
     showPlayerCount = true,                                -- If set to true the player count will be displayed in the rich presence
-    maxPlayers = 48,                                       -- Maximum amount of players
+    maxPlayers = 64,                                       -- Maximum amount of players
     buttons = {
         {
-            text = 'First Button!',
-            url = 'fivem://connect/localhost:30120'
+            text = 'About',
+            url = 'https://x.com/yukad2_/status/1745468102283022812?s=20'
         },
-        {
-            text = 'Second Button!',
-            url = 'fivem://connect/localhost:30120'
-        }
     }
 }
 
 Config.Density = {
-    parked = 0.5,
-    vehicle = 0.5,
-    multiplier = 0.5,
-    peds = 0.5,
-    scenario = 0.5
+    parked = 0.0,
+    vehicle = 0.0,
+    multiplier = 0.0,
+    peds = 0.0,
+    scenario = 0.0
 }
 
 Config.Disable = {
@@ -100,23 +96,67 @@ Config.Consumables = {
         ["casino_psqs"] = math.random(25, 40),
         ["casino_ego_chaser"] = math.random(25, 40),
         ["casino_sandwitch"] = math.random(25, 40),
-        ["casino_donut"] = math.random(25, 40)
+        ["casino_donut"] = math.random(25, 40),
+        -- cl-pizza fruit
+        ['porange'] =5,
+        ['papple'] =5,
+        ['pbanana'] =5,
+
+        -- cl-pizza pizza
+        ['pmacncheese'] =25,
+        ['pbbqporkmac'] =25,
+        ['pfresca'] =25,
+        ['pmargharitaslice'] =30,
+        ['pnapollitanoslice'] =30,
+        ['pmushroomspizzaslice'] =30,
+        ['pseafoodslice'] =30,
+        ['pvegpizzaslice'] =25,
     },
     drink = { -- default drink items
         ['water_bottle'] = math.random(35, 54),
         ['kurkakola'] = math.random(35, 54),
         ['coffee'] = math.random(40, 50),
         -- Casino Bar Drinks
-        ["casino_beer"] = math.random(25, 40),
         ["casino_coke"] = math.random(25, 40),
         ["casino_sprite"] = math.random(25, 40),
         ["casino_luckypotion"] = math.random(25, 40),
-        ["casino_coffee"] = math.random(25, 40)
+        ["casino_coffee"] = math.random(25, 40),
+        -- cl-pizza drinkable
+        ['pwatercup'] =5,
+        ['pespressomacchiato'] =5,
+        ['pcaramelfrappucino'] =5,
+        ['pcoldbrewlatte'] =5,
+        ['pstrawberryvanillaoatlatte'] =5,
+        ['pcocacola'] =5,
+        ['psprite'] =5,
+        ['ppepper'] =5,
     },
     alcohol = { -- default alcohol items
         ['whiskey'] = math.random(20, 30),
         ['beer'] = math.random(30, 40),
         ['vodka'] = math.random(20, 40),
+        -- Casino Bar Drinks
+        ["casino_beer"] = math.random(25, 40),
+        -- cl-pizza drinkable
+        ['pbeermugfull'] =5,
+        ['predwine'] =5,
+        ['pwhitewine'] =5,
+        ['ppinkwine'] =5,
+        ['pdusche'] =5,
+        ['plogger'] =5,
+        ['pam'] =5,
+        ['pwhiskey'] =5,
+        ['pgoldsake'] =5,
+        ['prum'] =5,
+        ['pwhitewinebottle'] =5,
+        ['pwhiskeybottle'] =5,
+        ['pchampagne'] =5,
+        ['predwinebottle'] =5,
+        ['psparklingwine'] =5,
+        ['pcastellobrolio'] =5,
+        ['pgaryfarrel'] =5,
+        ['prutherfordhill'] =5,
+        ['ppinkwinebottle'] =5,
     },
     custom = { -- put any custom items here
     -- ['newitem'] = {
