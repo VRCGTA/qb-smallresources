c000834383065660c0c06ab975ba8a33bcb1a594
azutake <azusa_takemae@dwango.co.jp>
2024-04-20 02:13:29 +0900

    fix by yuka

--- client/consumables.lua
+++ client/consumables.lua
@@ -205,7 +205,7 @@ RegisterNetEvent('consumables:client:DrinkAlcohol', function(itemName)
         TriggerEvent('inventory:client:ItemBox', QBCore.Shared.Items[itemName], 'remove')
         TriggerServerEvent('consumables:server:drinkAlcohol', itemName)
         TriggerServerEvent('consumables:server:addThirst', QBCore.Functions.GetPlayerData().metadata.thirst + Config.Consumables.alcohol[itemName])
-        TriggerServerEvent('hud:server:RelieveStress', math.random(10, 30))
+        TriggerServerEvent('hud:server:RelieveStress', 5)
         alcoholCount += 1
         AlcoholLoop()
         if alcoholCount > 1 and alcoholCount < 4 then
@@ -376,13 +376,13 @@ RegisterNetEvent('consumables:client:UseJoint', function()
         disableCombat = true,
     }, {}, {}, {}, function() -- Done
         TriggerEvent('inventory:client:ItemBox', QBCore.Shared.Items['joint'], 'remove')
-        TriggerServerEvent('hud:server:RelieveStress', math.random(30, 50))
+        TriggerServerEvent('hud:server:RelieveStress', math.random(25, 45))
         if IsPedInAnyVehicle(PlayerPedId(), false) then
             TriggerEvent('animations:client:EmoteCommandStart', { 'smoke3' })
         else
             TriggerEvent('animations:client:EmoteCommandStart', { 'smokeweed' })
         end
-        TriggerEvent('evidence:client:SetStatus', 'weedsmell', 300)
+        -- TriggerEvent('evidence:client:SetStatus', 'weedsmell', 300)
         TriggerEvent('animations:client:SmokeWeed')
     end)
 end)
@@ -395,13 +395,12 @@ RegisterNetEvent('consumables:client:UseOsatou', function()
         disableCombat = true,
     }, {}, {}, {}, function() -- Done
         TriggerEvent('inventory:client:ItemBox', QBCore.Shared.Items['seniti_osatou'], 'remove')
-        TriggerServerEvent('hud:server:RelieveStress', math.random(20, 40))
+        TriggerServerEvent('hud:server:RelieveStress', math.random(25, 45))
         if IsPedInAnyVehicle(PlayerPedId(), false) then
             TriggerEvent('animations:client:EmoteCommandStart', { 'smoke3' })
         else
             TriggerEvent('animations:client:EmoteCommandStart', { 'smokeweed' })
         end
-        -- TriggerEvent('evidence:client:SetStatus', 'weedsmell', 300)
         TriggerEvent('animations:client:SmokeWeed')
     end)
 end)
