f99048ed3b3c8b3f2b00d106deee081b493462dd
azutake <azusa_takemae@dwango.co.jp>
2024-05-01 23:29:02 +0900

    AFKのoff duty処理を修正

--- server/afk.lua
+++ server/afk.lua
@@ -2,8 +2,9 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('KickForAFK', function()
     local player = QBCore.Functions.GetPlayer(source)
+    if not player then return end
     if player.PlayerData.job.onduty then
-        TriggerEvent('QBCore:ToggleDuty')
+        player.Functions.SetJobDuty(false)
         TriggerClientEvent('ox_lib:alertDialog', source, {
             header = '放置中',
             content = '一定時間放置したため退勤状態になりました',
